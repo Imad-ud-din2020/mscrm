788315ecaa8b9c9a5965c684ba4010f376a0bd2e
Imad-ud-din2020 <imadcse100@gmail.com>
2021-02-01 10:53:44 +0530

    add else if loop

--- phonevalidation.js
+++ phonevalidation.js
@@ -7,6 +7,10 @@ var pattern = /^[0-9]\d{2,4}\d{6,8}$/;
 if (mobilenumber.match(pattern)) {
 return true;
 }
+else if(mobilenumber>10){
+    alert("number should be of 10 digit");
+}
+
 else {
 
 alert("Please Enter Valid Mobile NUmber");
